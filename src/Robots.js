3d601ad4103d70b349012bf92ab92c29fa510b0c
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2020-05-02 13:44:16 +1000

    searches robotname but no API

---
+++ src/Robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Ally",
+    username: "AllyOxley",
+    email: "ally@Febrary.com",
+  },
+
+  {
+    id: 2,
+    name: "Adam",
+    username: "AdamOxley",
+    email: "Adam@Febrary.com",
+  },
+  {
+    id: 3,
+    name: "Fed",
+    username: "FedOxley",
+    email: "Fed@Febrary.com",
+  },
+  {
+    id: 4,
+    name: "Sean",
+    username: "SeanOxley",
+    email: "Sean@Febrary.com",
+  },
+  {
+    id: 5,
+    name: "Elly",
+    username: "ellyOxley",
+    email: "elly@Febrary.com",
+  },
+
+  {
+    id: 6,
+    name: "edam",
+    username: "edamOxley",
+    email: "edam@Febrary.com",
+  },
+  {
+    id: 7,
+    name: "Fran",
+    username: "FranOxley",
+    email: "Fran@Febrary.com",
+  },
+  {
+    id: 8,
+    name: "Saean",
+    username: "SeanOxley",
+    email: "Saean@Febrary.com",
+  },
+  {
+    id: 9,
+    name: "RSean",
+    username: "rseanOxley",
+    email: "Srsean@Febrary.com",
+  },
+  {
+    id: 10,
+    name: "kkean",
+    username: "gSeanOxley",
+    email: "fSean@Febrary.com",
+  },
+];
