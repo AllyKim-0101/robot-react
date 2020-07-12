7d8369c9921775b41bf4237f485de33c53bc435e
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2020-07-12 12:04:19 +1000

    Updates

---
+++ precache-manifest.41ec9d0b14859b70915775e1dcea2a81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119b696d66d7a4043f9cee89850466d4",
+    "url": "/robot-react/index.html"
+  },
+  {
+    "revision": "eef8330d3f2117733a53",
+    "url": "/robot-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "84d6b99cb9d33b16f662",
+    "url": "/robot-react/static/css/main.bd57f80b.chunk.css"
+  },
+  {
+    "revision": "eef8330d3f2117733a53",
+    "url": "/robot-react/static/js/2.bc99f034.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-react/static/js/2.bc99f034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d6b99cb9d33b16f662",
+    "url": "/robot-react/static/js/main.074f5761.chunk.js"
+  },
+  {
+    "revision": "d95779fd41cf36da5faa",
+    "url": "/robot-react/static/js/runtime-main.3c0627cf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-react/static/media/SEGA_.12a3db8b.woff"
+  }
+]);
